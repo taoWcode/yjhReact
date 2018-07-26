8fd10a596e2654c421bd32c83e1f0b19ae54d339
weitao <itophp@itophp.com>
2018-07-26 20:10:43 +0800

    junshop商城React版本

---
+++ src/App/index.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2018-07-26 18:11:40
+ * @version $Id$
+ */
+import React from 'react';
+import './index.scss';
+
+
+export default class App extends React.Component{
+	render(){
+		return (<div className="yongjiuhui-app">
+					<p className = 'm-header'>头部</p>
+				</div>)
+	}
+}
